d54bc5a11afeff0400ce0f272c216cdbb3d28df0
CJ <csh.joo77@gmail.com>
2024-04-02 21:25:09 -0400

    added a readme file with screenshots and link to video.

--- Main/index.js
+++ Main/index.js
@@ -1,39 +1,38 @@
-const CLI = require("./lib/cli");
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Square, Circle, } = require('./lib/shapes');
+const { Triangle, Square, Circle } = require('./lib/shapes');
 
 class CLI {
-    async run() {
-      try {
-        const userInput = await inquirer.prompt([
-          {
-            type: 'input',
-            name: 'text',
-            message: 'Enter text (up to three characters):',
-            validate: function(input) {
-              return input.length <= 3 ? true : 'Text must be up to three characters.';
-            }
-          },
-          {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter text color (color keyword or hexadecimal number):'
-          },
-          {
-            type: 'list',
-            name: 'shape',
-            message: 'Select a shape:',
-            choices: ['Triangle', 'Square', 'Circle']
-          },
-          {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter shape color (color keyword or hexadecimal number):'
+  async run() {
+    try {
+      const userInput = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter text (up to three characters):',
+          validate: function(input) {
+            return input.length <= 3 ? true : 'Text must be up to three characters.';
           }
-        ]);
-        
-        let shape;
+        },
+        {
+          type: 'input',
+          name: 'textColor',
+          message: 'Enter text color (color keyword or hexadecimal number):'
+        },
+        {
+          type: 'list',
+          name: 'shape',
+          message: 'Select a shape:',
+          choices: ['Triangle', 'Square', 'Circle']
+        },
+        {
+          type: 'input',
+          name: 'shapeColor',
+          message: 'Enter shape color (color keyword or hexadecimal number):'
+        }
+      ]);
+      
+      let shape;
       switch (userInput.shape) {
         case 'Triangle':
           shape = new Triangle();
@@ -46,19 +45,17 @@ class CLI {
           break;
         default:
           throw new Error('Invalid shape selected');
-    }
+      }
 
-
-
-fs.writeFileSync('logo.svg', SVGTextContentElement);
-console.log('Generated SVG logo');
-} catch (error) {
-  console.error('An error has occured: ', error);
-}
+      shape.setColor(userInput.shapeColor);
+      const svgContent = shape.render(userInput.text, userInput.textColor);
+      
+      fs.writeFileSync('logo.svg', svgContent);
+      console.log('Generated SVG logo');
+    } catch (error) {
+      console.error('An error has occurred:', error);
     }
   }
-
-
-
+}
 
 new CLI().run();
